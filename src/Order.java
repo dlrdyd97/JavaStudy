66a8aa665676c6f20e8e888c7be4df8f19433434
dlrdyd97 <dlrdyd97@naver.com>
2023-10-10 15:53:28 +0900

    1

---
+++ src/Order.java
@@ -0,0 +1,9 @@
+public class Order {
+
+    int orderId;
+    String buyerId;
+    String sellerId;
+    int productId;
+    String orderDate;
+
+}
---
+++ src/ch01/Student.java
@@ -0,0 +1,12 @@
+package ch01;
+
+public class Student { // 클래스 이름의  시작은 대문자 , 파스칼표기법
+    // public을 가진 클래스는 자바파일 이름과 같아야 하고 단 하나만 가질 수 있음
+
+    int studentNumber; // 변수는 카멜 표기법
+    String studentName;
+    int majorCode;
+    String majorName;
+    int grade;
+
+}
