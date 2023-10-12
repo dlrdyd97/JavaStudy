fccf212e1d790c9b262c61c183a99a33f63ce873
dlrdyd97 <dlrdyd97@naver.com>
2023-10-12 23:34:57 +0900

    ch04

---
+++ src/ch04/StudentTest.java
@@ -0,0 +1,16 @@
+package ch04;
+
+public class StudentTest {
+
+    public static void main(String[] args){
+
+        Student studentLee=new Student();
+        studentLee.studentId=166;
+        studentLee.setStudentName("김익환");
+        studentLee.address="경기도 안양시";
+
+        studentLee.showStudentInfo();
+
+    }
+
+}
