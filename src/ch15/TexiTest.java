3d8d2ba44145143511a97cbe759ecfbe17569b77
dlrdyd97 <dlrdyd97@naver.com>
2023-10-14 18:26:50 +0900

    ch15

--- src/ch15/TexiTest.java
+++ src/ch15/TexiTest.java
@@ -5,12 +5,12 @@ public class TexiTest {
     public static void main(String[] args){
 
         Person personK=new Person("Kim",30000);
-        Taxi texihappy=new Taxi("해피");
+        Taxi taxihappy=new Taxi("해피");
 
-        personK.takeTaxi(texihappy,10000);
+        personK.takeTaxi(taxihappy,10000);
 
         personK.showPersonInfo();
-        texihappy.showTaxiInfo();
+        taxihappy.showTaxiInfo();
 
     }
 
