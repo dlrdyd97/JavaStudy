245ab4f009ff7d188ed13bd1ba07da273ee465a2
dlrdyd97 <dlrdyd97@naver.com>
2023-10-16 13:08:35 +0900

    ch18

--- src/ch16/EmployeeTest.java
+++ src/ch16/EmployeeTest.java
@@ -7,11 +7,9 @@ public class EmployeeTest {
         Employee employeeKim = new Employee();
         employeeKim.setEmployeeName("Kim");
 
-        System.out.println(employeeKim.serialNum);
 
         Employee employeeKang = new Employee();
         employeeKang.setEmployeeName("강태혁");
-        employeeKang.serialNum++;
 
         System.out.println(employeeKim.getEmployeeName()+"님의 사번은 "+employeeKim.getEmployeeId());
         System.out.println(employeeKang.getEmployeeName()+"님의 사번은 "+employeeKang.getEmployeeId());
---
+++ src/ch18/Company.java
@@ -0,0 +1,21 @@
+package ch18;
+
+public class Company {
+
+    private Company() {
+
+    }
+
+    private static Company instance=new Company();
+
+    public static Company getInstance(){
+
+        if(instance==null){
+            instance=new Company();
+        }
+
+        return instance;
+
+    }
+
+}
