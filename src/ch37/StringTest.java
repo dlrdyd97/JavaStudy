1832abc21572b7863e9d207675a5c432e328d9e0
dlrdyd97 <dlrdyd97@naver.com>
2023-10-19 22:45:51 +0900

    ch37 String, StringBuilder, StringBuffer 클래스, text block

---
+++ src/ch37/StringTest.java
@@ -0,0 +1,25 @@
+package ch37;
+
+public class StringTest {
+
+    public static void main(String[] args){
+
+        String str1=new String("abc");
+        String str2=new String("abc");
+        System.out.println(str1==str2);
+        System.out.println(str1.equals(str2));
+
+        String str3="abc";
+        String str4="abc";
+        System.out.println(str3==str4);
+        System.out.println(str3.equals(str4));
+
+        System.out.println(System.identityHashCode(str1));
+        System.out.println(System.identityHashCode(str2));
+        System.out.println(System.identityHashCode(str3));
+        System.out.println(System.identityHashCode(str4));
+
+
+    }
+
+}
