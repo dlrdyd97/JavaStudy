faf766ffea46e5bff035948175854beacb369cc3
dlrdyd97 <dlrdyd97@naver.com>
2023-10-23 13:45:56 +0900

    ch40 Collection 요소를 순회하는 Iterator

---
+++ src/ch40/Member.java
@@ -0,0 +1,23 @@
+package ch40;
+
+public class Member {
+
+    int memberId;
+    String memberName;
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+}
---
+++ src/ch40/MemberArrayList.java
@@ -0,0 +1,50 @@
+package ch40;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MemberArrayList {
+
+    private ArrayList<Member> arrayList;
+
+    public MemberArrayList(int size) {
+        arrayList = new ArrayList<>();
+    }
+
+    public void addMember(Member member){
+        arrayList.add(member);
+    }
+
+//    public boolean removeMember(int memberId){
+//
+//        for(int i=0;i<arrayList.size();i++){
+//            Member member= arrayList.get(i);
+//
+//            int tempId=member.getMemberId();
+//            if(tempId==memberId){
+//                arrayList.remove(i);
+//                return true;
+//            }
+//        }
+//
+//        System.out.println(memberId+"가 존재하지 않습니다.");
+//        return false;
+//
+//    }
+
+    public boolean removeMember(int memberId){
+        Iterator<Member> ir=arrayList.iterator();
+        while ((ir.hasNext())){
+            Member member=ir.next();
+            int tempId=member.getMemberId();
+            if(tempId==memberId){
+                arrayList.remove(member);
+                return true;
+            }
+        }
+
+        System.out.println(memberId+"가 존재하지 않습니다.");
+        return false;
+    }
+
+}
