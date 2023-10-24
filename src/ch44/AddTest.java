e012277f611637527cf0107cb16c72114bc146be
dlrdyd97 <dlrdyd97@naver.com>
2023-10-24 23:38:44 +0900

    ch44 Lambda 람다 표현식

--- src/ch44/AddTest.java
+++ src/ch44/AddTest.java
@@ -4,22 +4,19 @@ public class AddTest {
 
     public static void main(String[] args){
 
-        Add add=(x,y)->{return x+y;};
+//        Add add=(x,y)->{return x+y;};
+        Add add=(x,y)-> x+y;
 
         System.out.println(add.add(2,3));
 
-        MyNumber myNumber=(x,y)->{
-            return Math.max(x,y);
-        };
+        MyNumber myNumber=(x,y)->Math.max(x,y);
 
         System.out.println(myNumber.getMax(1,10));
 
         StringConcat sc=new StrungConcatImpl();
         sc.makeString("함","형우");
 
-        StringConcat sc1=(s1,s2)->{
-            System.out.println(s1+","+s2);
-        };
+        StringConcat sc1=(s1,s2)->System.out.println(s1+","+s2);
         sc1.makeString("김","익환");
 
         StringConcat sc2=new StringConcat() {
