a0b2561b7c0044f311d43e95b324f2b46451f5b7
dlrdyd97 <dlrdyd97@naver.com>
2023-10-25 19:49:56 +0900

    ch47 연산 수행에 대한 구현을 할 수 있는 reduce()연산

---
+++ src/ch47/ReduceTest.java
@@ -0,0 +1,37 @@
+package ch47;
+
+import java.util.Arrays;
+import java.util.function.BinaryOperator;
+
+class CompareString implements BinaryOperator<String> {
+
+    @Override
+    public String apply(String s1, String s2) {
+        if (s1.getBytes().length >= s2.getBytes().length) return s1;
+        else return s2;
+    }
+}
+
+
+public class ReduceTest {
+
+    public static void main(String[] args){
+
+        String greeting[]= {"안녕하세요~~~", "hello", "Good morning", "반갑습니다^^"};
+//        String greeting[]= {""};
+
+        System.out.println(Arrays.stream(greeting).reduce("",(s1,s2)->
+                {if(s1.length()>=s2.length())
+                    return s1;
+                    else
+                        return s2;
+                }
+                ));
+
+        String str = Arrays.stream(greeting).reduce(new CompareString()).get(); //BinaryOperator를 구현한 클래스 이용
+        System.out.println(str);
+
+
+    }
+
+}
