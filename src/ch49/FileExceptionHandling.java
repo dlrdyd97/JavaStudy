0445d6cc4c0e0ed71219622116d3c35ccf624b6f
dlrdyd97 <dlrdyd97@naver.com>
2023-10-27 16:50:45 +0900

    ch49 예외 처리 Try Catch

--- src/ch49/FileExceptionHandling.java
+++ src/ch49/FileExceptionHandling.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class FileExceptionHandling {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
 
         FileInputStream fis=null;
         try {
@@ -19,6 +19,7 @@ public class FileExceptionHandling {
             }
         } catch (FileNotFoundException e) {
             System.out.println(e);
+            return; // 리턴이 존재해도 finally는 항상 실행됨
         }finally {
             if(fis!=null){
                 try {
@@ -31,6 +32,15 @@ public class FileExceptionHandling {
             System.out.println("항상 수행 됩니다.");
         }
         System.out.println("여기도 수행됩니다");
+
+//        try(FileInputStream fis=new FileInputStream("a.txt")) {
+//            System.out.println("read");
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+//        System.out.println("end");
 
     }
 
