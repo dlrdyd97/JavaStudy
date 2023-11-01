d1065cf66819359138430cd6c69e6fe28c2b1c79
dlrdyd97 <dlrdyd97@naver.com>
2023-11-01 14:01:00 +0900

    ch51 오류의 로그를 남기기 - java.util.logging.Logger 활용

---
+++ src/ch51/LoggerTest.java
@@ -0,0 +1,12 @@
+package ch51;
+
+public class LoggerTest {
+
+    public static void main(String[] args){
+
+        MyLogger logger=MyLogger.getLogger();
+        logger.log("log test");
+
+    }
+
+}
