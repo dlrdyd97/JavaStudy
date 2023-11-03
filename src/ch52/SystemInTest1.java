8218b2663b9ac9dfd738038f25af8c2b42984e5f
dlrdyd97 <dlrdyd97@naver.com>
2023-11-03 16:28:47 +0900

    ch52 자바의 입출력을 위한 I/O 스트림

---
+++ src/ch52/SystemInTest1.java
@@ -0,0 +1,27 @@
+package ch52;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SystemInTest1 {
+
+    public static void main(String[] args){
+
+        System.out.println("알파벳 하나를 쓰고 [Enter]를 누르세요");
+
+        int i;
+
+        try {
+            InputStreamReader irs=new InputStreamReader(System.in);
+            while ((i=irs.read()) !='\n') {
+//                System.out.println(i);
+                System.out.print((char) i);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
